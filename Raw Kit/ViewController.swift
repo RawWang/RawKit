7f224381d3aabc8d7e327409cd0a88a85efbb013
Raw Wang <Zhao.Wang@Honeywell.com>
2020-03-12 14:32:50 +0800

    Initialize the project.

---
+++ Raw Kit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Raw Kit
+//
+//  Created by SW on 2020/3/12.
+//  Copyright © 2020 Raw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
